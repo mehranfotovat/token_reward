9aa6992b6116e89a9c991d14eecf9c09220c64c7
Mehran <mehr4nfotovat@gmail.com>
2024-07-06 19:08:02 +0330

    token has been minted without PDA

--- programs/token_reward/src/lib.rs
+++ programs/token_reward/src/lib.rs
@@ -1,22 +1,38 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount, MintTo};
 
 declare_id!("E9yxiWvqXm76keKtxZ5MFQpdpJfDJThgvX85tpVWdzPQ");
 
 #[program]
 pub mod token_reward {
+    use anchor_spl::token;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<MintToken>, token_name: String, token_amount: u64) -> Result<()> {
+    pub fn initialize(ctx: Context<MintTokenPDA>, token_name: String, token_amount: u64) -> Result<()> {
         let token_data = &mut ctx.accounts.token_mint;
         token_data.token_name = token_name;
         token_data.token_amount = token_amount;
         msg!("token has been Created!");
         Ok(())
     }
+
+    pub fn mint_token(ctx: Context<MintToken>, amount: u64) -> Result<()> {
+        let cpi_account = MintTo{
+            mint: ctx.accounts.token_mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info()
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_account);
+
+        token::mint_to(cpi_ctx, amount)?;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct MintToken<'info> {
+pub struct MintTokenPDA<'info> {
     #[account(
         init,
         seeds = [payer.key().as_ref()],
@@ -34,4 +50,16 @@ pub struct MintToken<'info> {
 pub struct MyToken {
     token_name: String,
     token_amount: u64,
+}
+
+#[derive(Accounts)]
+pub struct MintToken<'info> {
+    #[account(mut)]
+    pub token_mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    #[account(mut)]
+    pub token_account: Account<'info, TokenAccount>,
+    /// CHECK: payer account (authority) 
+    #[account(mut)]
+    pub payer: Signer<'info>,
 }
--- tests/token_reward.ts
+++ tests/token_reward.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { TokenReward } from "../target/types/token_reward";
-import { createMint, createAccount as createAccountSpl, createAssociatedTokenAccount, getOrCreateAssociatedTokenAccount, mintTo, transfer } from "@solana/spl-token";
+import { createMint, createAccount as createAccountSpl, createAssociatedTokenAccount, getOrCreateAssociatedTokenAccount, mintTo, transfer, getAssociatedTokenAddress, TOKEN_PROGRAM_ID, MINT_SIZE, createInitializeMintInstruction, createAssociatedTokenAccountInstruction } from "@solana/spl-token";
 
 describe("token_reward", () => {
   const provider = anchor.AnchorProvider.env();
@@ -22,20 +22,95 @@ describe("token_reward", () => {
 
   });
 
-  it("Mint Token and transfer using client!", async () => {
-    console.log("payer address", payer.publicKey.toBase58());
+  // it("Mint Token and transfer using client!", async () => {
+  //   console.log("payer address", payer.publicKey.toBase58());
 
-    // token mint is the account that holds data about token 
-    // this will return a pubkey for token mint
+  //   // token mint is the account that holds data about token 
+  //   // this will return a pubkey for token mint
+  //   const tokenMint = await createMint(
+  //     provider.connection,
+  //     payer,
+  //     payer.publicKey,
+  //     null, //payer.publicKey,
+  //     DECIMAL,
+  //   );
+  //   console.log("Token Mint", tokenMint.toBase58());
+
+  //   const tokenAccount = await getOrCreateAssociatedTokenAccount(
+  //     provider.connection,
+  //     payer,
+  //     tokenMint,
+  //     payer.publicKey,
+  //   )
+
+  //   console.log("Token Account: ", tokenAccount.address.toBase58())
+
+  //   // Our token has two decimal places
+  //   const MINOR_UNITS_PER_MAJOR_UNITS = Math.pow(10, 6);
+
+  //   const transactionSignature = await mintTo(
+  //     provider.connection,
+  //     payer,
+  //     tokenMint,
+  //     tokenAccount.address,
+  //     payer.publicKey,
+  //     100 * MINOR_UNITS_PER_MAJOR_UNITS
+  //   );
+
+  //   console.log('transactionSignature of minting to associate token account: ', transactionSignature);
+
+  //   const tokenRecieverAddress = new anchor.web3.Keypair();
+  
+  //   const tokenRecieverTokenAddress = await getOrCreateAssociatedTokenAccount(
+  //     provider.connection,
+  //     payer,
+  //     tokenMint,
+  //     tokenRecieverAddress.publicKey
+  //   );
+  
+  //   console.log("token reciever ata account: ", tokenRecieverTokenAddress.address.toBase58());
+
+  //   const transferSignature = await transfer(
+  //     provider.connection,
+  //     payer,
+  //     tokenAccount.address,
+  //     tokenRecieverTokenAddress.address,
+  //     payer.publicKey,
+  //     5 * MINOR_UNITS_PER_MAJOR_UNITS
+  //   );
+
+  //   console.log("transaction of token transfer: ", transactionSignature)
+  
+  // });
+
+  // it("Create PDA mint and transfer nft!", async () => {
+  //   const [tokenPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+  //     [payer.publicKey.toBuffer()],
+  //     program.programId
+  //   );
+
+  //   const creatingPDAProgramTx = await program.methods.initialize("Mehran Token", new anchor.BN(1000)).accounts({
+  //     tokenMint: tokenPDA,
+  //     payer: payer.publicKey
+  //   }).signers([payer]).rpc();
+
+  //   console.log(await program.account.myToken.fetch(tokenPDA.toBase58()));
+  //   console.log(creatingPDAProgramTx)
+  //   console.log(tokenPDA.toBase58())
+  // });
+
+  it("Create Token With Program!", async () => {
+    const MINOR_UNITS_PER_MAJOR_UNITS = Math.pow(10, 6);
+
+    //TODO: token mint
     const tokenMint = await createMint(
       provider.connection,
       payer,
       payer.publicKey,
-      null, //payer.publicKey,
-      DECIMAL,
+      null,
+      DECIMAL
     );
-    console.log("Token Mint", tokenMint.toBase58());
-
+    //TODO: ata
     const tokenAccount = await getOrCreateAssociatedTokenAccount(
       provider.connection,
       payer,
@@ -43,59 +118,15 @@ describe("token_reward", () => {
       payer.publicKey,
     )
 
-    console.log("Token Account: ", tokenAccount.address.toBase58())
+    console.log("Payer account: ", payer.publicKey.toBase58());
+    console.log("My program token Mint address: ", tokenMint.toBase58());
+    console.log("My ATA account: ", tokenAccount.address.toBase58());
 
-    // Our token has two decimal places
-    const MINOR_UNITS_PER_MAJOR_UNITS = Math.pow(10, 6);
-
-    const transactionSignature = await mintTo(
-      provider.connection,
-      payer,
-      tokenMint,
-      tokenAccount.address,
-      payer.publicKey,
-      100 * MINOR_UNITS_PER_MAJOR_UNITS
-    );
-
-    console.log('transactionSignature of minting to associate token account: ', transactionSignature);
-
-    const tokenRecieverAddress = new anchor.web3.Keypair();
-  
-    const tokenRecieverTokenAddress = await getOrCreateAssociatedTokenAccount(
-      provider.connection,
-      payer,
-      tokenMint,
-      tokenRecieverAddress.publicKey
-    );
-  
-    console.log("token reciever ata account: ", tokenRecieverTokenAddress.address.toBase58());
-
-    const transferSignature = await transfer(
-      provider.connection,
-      payer,
-      tokenAccount.address,
-      tokenRecieverTokenAddress.address,
-      payer.publicKey,
-      5 * MINOR_UNITS_PER_MAJOR_UNITS
-    );
-
-    console.log("transaction of token transfer: ", transactionSignature)
-  
-  });
-
-  it("Create PDA mint and transfer nft!", async () => {
-    const [tokenPDA] = anchor.web3.PublicKey.findProgramAddressSync(
-      [payer.publicKey.toBuffer()],
-      program.programId
-    );
-
-    const creatingPDAProgramTx = await program.methods.initialize("Mehran Token", new anchor.BN(1000)).accounts({
-      tokenMint: tokenPDA,
+    const mintTokenTx = await program.methods.mintToken(new anchor.BN(100 * MINOR_UNITS_PER_MAJOR_UNITS)).accounts({
+      tokenMint: tokenMint,
+      tokenAccount: tokenAccount.address,
       payer: payer.publicKey
     }).signers([payer]).rpc();
-
-    console.log(await program.account.myToken.fetch(tokenPDA.toBase58()));
-    console.log(creatingPDAProgramTx)
-    console.log(tokenPDA.toBase58())
+    console.log(mintTokenTx);
   });
 });
